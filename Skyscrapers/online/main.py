7bcecab48bad8fd2ce3f320e62fa1e8ce26c94a1
Torbjorn Söderberg <t.soderberg8@gmail.com>
2021-12-01 20:20:28 +0100

    top bar now uses position: fixed (does not move when scrolling)

--- Skyscrapers/online/main.py
+++ Skyscrapers/online/main.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
 			kwargs['players'] = globals()['players']
 	
 	tsobg.newGame(SkyscrapersGame(), int(nPlayers), **kwargs)
-	tsobg.runServer(False)
+	tsobg.runServer(True)
 	
 	
